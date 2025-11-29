71e45762ef38b0bf5d4c40b636c7eb05355a433b
harshitachhabra2104 <harshibarbie490@gmail.com>
2025-11-29 20:46:54 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chat from "./Chat";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>MediMind AI</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
